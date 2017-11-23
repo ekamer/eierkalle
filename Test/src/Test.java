a7a3bd91b6ccdb680521c5b39cce7a0f6a7096c2
alex <alex@Sieglind>
2017-11-23 10:52:21 +0100

    cock

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
-public class Test {    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        //int i=5;
-        
-        System.out.println("Gib Größe");
-        double gr = sc.nextInt();
-        System.out.println("Gib Gewicht");
-        int gew = sc.nextInt();
-        
-        double x = (gew / ((gr / 100) * (gr / 100)));
-        
-        System.out.println("Ihr BMI ist:" + x);
-        
-        if(x>25)
-        	System.out.println("Sie sind Übergwewichtig!");
-        
-    }
-            
+
+public class Test {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// int i=5;
+
+		System.out.println("Gib Grï¿½ï¿½e");
+		double gr = sc.nextInt();
+		System.out.println("Gib Gewicht");
+		int gew = sc.nextInt();
+
+		double x = (gew / ((gr / 100) * (gr / 100)));
+
+		System.out.println("Ihr BMI ist:" + x);
+
+		if (x > 25)
+			System.out.println("Sie sind uebergwewichtig!");
+
+	}
+
 }
